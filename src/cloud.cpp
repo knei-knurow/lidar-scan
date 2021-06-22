d76a5fc3a7a86b5aabb25f85cfb2475b681b4edb
Bartek Pacia <barpac02@gmail.com>
2021-06-22 11:18:38 +0200

    format code with new .clang-format

--- src/cloud.cpp
+++ src/cloud.cpp
@@ -4,13 +4,17 @@
 // PointCyl
 //
 PointCart PointCyl::to_cart() const {
-  int x = int(std::round(dist * std::sin(angle * (acos(-1.0f) / 180.0f))));
-  int y = int(std::round(dist * std::cos(angle * (acos(-1.0f) / 180.0f))));
-  return {x, y};
+    x_cart = dist * std::sin(angle * (acos(-1.0f) / 180.0f));
+    y_cart = dist * std::cos(angle * (acos(-1.0f) / 180.0f));
+    int x = int(std::round(x_cart));
+    int y = int(std::round(y_cart));
+    return {x, y};
 }
 
 PointCart PointCyl::to_cart(float scale, float origin_x, float origin_y) const {
-  int x = int(std::round(dist * std::sin(angle * (acos(-1.0f) / 180.0f)) * scale)) + origin_x;
-  int y = int(std::round(dist * std::cos(angle * (acos(-1.0f) / 180.0f)) * scale)) + origin_y;
-  return {x, y};
+    x_cyl = dist * std::sin(angle * (acos(-1.0f) / 180.0f)) * scale;
+    y_cyl = dist * std::cos(angle * (acos(-1.0f) / 180.0f)) * scale;
+    int x = int(std::round(x_cyl)) + origin_x;
+    int y = int(std::round(y_cyl)) + origin_y;
+    return {x, y};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 #include "../include/app.hpp"
 
-#if defined(__unix__) || defined(__unix) || ((defined(__APPLE__) && defined(__MACH__)))
+#if defined(__unix__) || defined(__unix) || \
+    ((defined(__APPLE__) && defined(__MACH__)))
 #define UNIX_LIKE
 #endif
 
@@ -12,15 +13,15 @@ std::unique_ptr<App> app;
 #ifdef _WIN32
 #include <Windows.h>
 BOOL WINAPI ctrl_c_handler(DWORD signal) {
-  if (signal == CTRL_C_EVENT) {
-    if (app) {
-      app->close();
-      return TRUE;
-    } else {
-      return FALSE;
+    if (signal == CTRL_C_EVENT) {
+        if (app) {
+            app->close();
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
-  }
-  return TRUE;
+    return TRUE;
 }
 
 #endif
@@ -29,31 +30,31 @@ BOOL WINAPI ctrl_c_handler(DWORD signal) {
 #include <csignal>
 #include <cstdlib>
 void ctrl_c_handler(int signal) {
-  std::cout << "closing using ctrl+c" << std::endl;
-  app->close();
+    std::cout << "closing using ctrl+c" << std::endl;
+    app->close();
 }
 #endif
 
 int main(int argc, char** argv) {
 #ifdef _WIN32
-  if (!SetConsoleCtrlHandler(ctrl_c_handler, TRUE)) {
-    return -1;
-  }
+    if (!SetConsoleCtrlHandler(ctrl_c_handler, TRUE)) {
+        return -1;
+    }
 #endif
 #ifdef UNIX_LIKE
 #include <csignal>
 #include <cstdlib>
-  struct sigaction handler;
-  handler.sa_handler = ctrl_c_handler;
-  sigemptyset(&handler.sa_mask);
-  handler.sa_flags = 0;
-  sigaction(SIGINT, &handler, NULL);
+    struct sigaction handler;
+    handler.sa_handler = ctrl_c_handler;
+    sigemptyset(&handler.sa_mask);
+    handler.sa_flags = 0;
+    sigaction(SIGINT, &handler, NULL);
 #endif
 
-  std::vector<std::string> args;
-  for (int i = 1; i < argc; i++) {
-    args.push_back(std::string(argv[i]));
-  }
-  app = std::make_unique<App>(args);
-  return app->run();
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; i++) {
+        args.push_back(std::string(argv[i]));
+    }
+    app = std::make_unique<App>(args);
+    return app->run();
 }
